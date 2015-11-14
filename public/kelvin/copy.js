d268f2d93795108dc9adab0420943c5a15a6c30e
Kelvin Yang <hjyang@yahoo-inc.com>
2015-11-15 00:54:02 +0800

    refactor

--- public/kelvin/copy.js
+++ public/kelvin/copy.js
@@ -7,26 +7,7 @@ function thisShouldWork() {
             xfbml      : true,
             version    : API_VERSION
         });
-
-        FB.getLoginStatus(function(response) {
-            if (response.status === 'connected') {
-                // the user is logged in and has authenticated your
-                // app, and response.authResponse supplies
-                // the user's ID, a valid access token, a signed
-                // request, and the time the access token 
-                // and signed request each expire
-                var uid = response.authResponse.userID;
-                var accessToken = response.authResponse.accessToken;
-
-                doTheThing();
-            } else if (response.status === 'not_authorized') {
-                // the user is logged in to Facebook, 
-                // but has not authenticated your app
-                loginAndDoThing();
-            } else {
-                loginAndDoThing();
-            }
-        });
+        loginIfNecessaryAndCall(doTheThing);
     };
 
     (function(d, s, id){
