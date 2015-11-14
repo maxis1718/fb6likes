c07146db649308c00a12bad9ffabbca0872a5d01
Maxis Kao <maxiskao@yahoo-inc.com>
2015-11-14 21:44:04 +0800

    route

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.redirect('/index.html');
 });
 
 app.listen(app.get('port'), function() {
