b06a9f2ba3de232660be48c90ff95bf481f600b8
Maxis Kao <maxiskao@yahoo-inc.com>
2015-11-14 21:49:17 +0800

    clean up 102

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
 app.get('/', function(req, res) {
 	res.redirect('/index.html');
 });
