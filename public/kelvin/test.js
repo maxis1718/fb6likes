c2a808f4f4636f24866ca5ad2fb1f140730dd51f
Kelvin Yang <hjyang@yahoo-inc.com>
2015-11-15 03:33:42 +0800

    parsed afa likes.

--- public/kelvin/test.js
+++ public/kelvin/test.js
@@ -21,15 +21,30 @@ function promisedRequest(req) {
 }
 
 function fetchMyFriend() {
+    var fieldWeCare = [
+        'name',
+        'id',
+        'picture'
+    ];
     return new Promise(function(resolve,reject) {
-        var fieldWeCare = [
-            'name',
-            'id',
-            'picture'
-        ];
         promisedRequest('/me/friends?fields=' + fieldWeCare.join(',')).then(
             function(res) {
                 resolve(res.data);
+            }
+        );
+    });
+}
+
+function fetchMe() {
+    var fieldWeCare = [
+        'name',
+        'id',
+        'picture'
+    ];
+    return new Promise(function(resolve,reject) {
+        promisedRequest('/me?fields=' + fieldWeCare.join(',')).then(
+            function(res) {
+                resolve(res);
             }
         );
     });
@@ -41,7 +56,7 @@ function preprocessFeedArr(arr) {
     });
 }
 
-function fetchFeed(uid) {
+function fetchFeed(uid, regexMatcher) {
     var fieldWeCare = [
         'message',
         'link',
@@ -55,6 +70,10 @@ function fetchFeed(uid) {
     var req = '/' + uid + '/feed?fields=' + fieldWeCare.join(',') + '&limit=' + limit;
     return promisedRequest(req).then(function(res) {
         var feedArr = res.data;
+        feedArr = feedArr.filter(function(f) {
+            var str = f.message + f.description + f.story + f.caption;
+            return str && str.match && !!str.match(regexMatcher);
+        });
         preprocessFeedArr(feedArr);
         return {
             uid:uid,
@@ -63,10 +82,15 @@ function fetchFeed(uid) {
     });
 }
 
-function fetchAllFeed(targets) {
-    var ids = ['me'].concat(targets.map(function(x) { return x.id; }));
-    var reqs = ids.map(function(id) { return fetchFeed(id); });
-    return Promise.all(reqs);
+function feedFetcher(regexMatcher) {
+    return {
+        fetchAllFeed: function(targets) {
+            //var ids = ['me'].concat(targets.map(function(x) { return x.id; }));
+            var ids = targets.map(function(x) { return x.id; });
+            var reqs = ids.map(function(id) { return fetchFeed(id, regexMatcher); });
+            return Promise.all(reqs);
+        }
+    };
 }
 
 function genDistance(data) {
@@ -84,6 +108,8 @@ function genDistance(data) {
     // ]
     var n = data.length;
     var score = Array.apply(null, Array(n)).map(Number.prototype.valueOf,0);
+    var id2index = {};
+    var ids;
     var posts = {
         // id -> {
         //   message: ...
@@ -91,14 +117,34 @@ function genDistance(data) {
         //   ...
         // }
     };
+    // id2index
+    ids = data.map(function(x) { return x.uid; });
+    for(var i=0; i<n; i++) {
+        id2index[data[i].uid] = i;
+    }
+    console.log(ids);
+    console.log(id2index);
     // fill posts object
     data.forEach(function(ele) {
         var feeds = ele.feed;
         feeds.forEach(function(feed) {
-            posts[feed.md5] = _.clone(feed);
+            var p = posts[feed.md5] = _.clone(feed);
+            p.likes = p.likes.data.map(function(x) { return x.id; });
+            p.likes = p.likes.filter(function(x) { return ids.indexOf(x)>=0; });
+            //console.log(p.likes.length);
+            console.log(p.likes);
         });
     });
-    console.log(posts);
+    //
+    for(var md5 in posts) {
+        var post = posts[md5];
+        var likes = post.likes;
+        //console.log(post);
+        //console.log(likes);
+        likes.forEach(function(id) {
+            
+        });
+    }
     // calc score
     // return format
     // ret: [
@@ -112,10 +158,14 @@ function genDistance(data) {
 }
 
 function test() {
+    var queryTerm = '馬習會';
+    //var regexMatcher = /馬習|馬囧|賣國/;
+    var regexMatcher = /台灣|中國|兩岸/;
     //testRequest('/me/friends');
-    fetchMyFriend().
+    Promise.all([fetchMe(), fetchMyFriend()]).
+    then(function(ans) { return [ans[0]].concat(ans[1]); }).
     then(dumpRes).
-    then(fetchAllFeed).
+    then(feedFetcher(regexMatcher).fetchAllFeed).
     then(dumpRes).
     then(genDistance);
     //testRequest('/me/feed');
