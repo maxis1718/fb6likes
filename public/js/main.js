ec366c868903df444a9efb93d1d38671cee6d184
Maxis Kao <maxiskao@yahoo-inc.com>
2015-11-15 05:01:08 +0800

    utilize fullPage and add input bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,11 @@
 
 
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        sectionsColor: ['#344b8d', '#233468', 'white'],
+        anchors: ['home', 'query', 'graph'],
+        scrollingSpeed: 1000
+    });
 
     $('.fb-logout-btn').click(function(e){
         FB.logout(function() {
